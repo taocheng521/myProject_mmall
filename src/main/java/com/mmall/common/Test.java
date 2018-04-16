1bdb31a0cfc7cad9f7701ff487f84de98a86265e
xiaobing <2087210527@qq.com>
2018-04-16 22:29:59 +0800

    ssss

---
+++ src/main/java/com/mmall/common/Test.java
@@ -0,0 +1,8 @@
+package com.mmall.common;
+
+/**
+ * Created by Administrator on 2018/4/16 0016.
+ */
+public class Test {
+    
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<h2>i love you</h2>
+</body>
+</html>
