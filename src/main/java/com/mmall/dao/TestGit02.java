6c17271a6544638916a3514a4c26731f30e1fb94
xiaobing <2087210527@qq.com>
2018-04-16 23:07:02 +0800

    修改

--- src/main/java/com/mmall/dao/TestGit02.java
+++ src/main/java/com/mmall/dao/TestGit02.java
@@ -4,4 +4,7 @@ package com.mmall.dao;
  * Created by Administrator on 2018/4/16 0016.
  */
 public class TestGit02 {
+    public static void main(String[] args) {
+        System.out.println("i love you");
+    }
 }
