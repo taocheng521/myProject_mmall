a9dfd4cc0240635fde91fe980f04214224c8a5d2
xiaobing <2087210527@qq.com>
2018-04-16 22:42:41 +0800

    增加方法

--- src/main/java/com/mmall/vo/TestGit06.java
+++ src/main/java/com/mmall/vo/TestGit06.java
@@ -4,4 +4,7 @@ package com.mmall.vo;
  * Created by Administrator on 2018/4/16 0016.
  */
 public class TestGit06 {
+    public static void main(String[] args) {
+        System.out.println();
+    }
 }
